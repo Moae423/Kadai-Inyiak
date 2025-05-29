113ab6fe46cad8e8d174f8d0bb6d1ccc09eec2ba
daffa <hanura423@gmail.com>
2025-05-29 09:01:55 +0700

    Vercel : Connect To DatabasE Postgres in vercel via Neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default function Home() {
+  return (
+    <div className="container py-6 -4 mx-auto">
+      <h1 className="text-2xl font-semibold">Homepage</h1>
+    </div>
+  );
 }
